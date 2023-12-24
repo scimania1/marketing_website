02a2d1c04a5197f1aa0bae7374a19ad258111e87
scimania <gauravdeepubhi@gmail.com>
2023-12-24 13:27:59 +0530

    refactor(search-bar): refactored the working of search-bar and made some UI changes

--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -1,60 +1,74 @@
 "use client";
 
-import useDebounce from "@/hooks/useDebounce";
 import createUrl from "@/utils/createUrl";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useRef } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
-export default function SearchBar({ initialQuery }: { initialQuery: string }) {
-  const [query, setQuery] = useState(initialQuery);
-  const prevQuery = useRef(initialQuery);
-  const debouncedQuery = useDebounce(query, 400);
-
+export default function SearchBar({ placeholder }: { placeholder: string }) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const crossRef = useRef<SVGSVGElement>(null);
   const router = useRouter();
   const searchParams = useSearchParams();
-
-  const initialRender = useRef(true);
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (initialRender.current) {
-      initialRender.current = false;
-      return;
-    }
-    const newParams = new URLSearchParams(searchParams.toString());
-    if (debouncedQuery.length === 0) {
-      newParams.delete("query");
+  const pathname = usePathname();
+  const handleSearch = useDebouncedCallback((query: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (query.length === 0) {
+      params.delete("query");
     } else {
-      newParams.set("query", debouncedQuery);
+      params.set("query", query);
     }
-    if (debouncedQuery !== prevQuery.current) {
-      newParams.set("page", "1");
-      prevQuery.current = debouncedQuery;
-    }
-    router.push(createUrl("/products", newParams));
-  }, [debouncedQuery, router, searchParams]);
-
+    params.delete("page");
+    router.push(createUrl(pathname, params));
+  }, 300);
   return (
     <div
-      className="flex flex-grow items-center gap-2 rounded-full bg-slate-200 py-3 pl-5 pr-6 focus-within:outline focus-within:outline-1 focus-within:outline-black sm:flex-grow-0"
+      className="test relative flex flex-grow items-center gap-2 rounded-full bg-slate-200 py-2 pl-5 pr-6 transition-colors duration-300 focus-within:outline-1 focus-within:outline-black sm:flex-grow-0 sm:py-3 md:py-4"
       role="searchbox"
     >
+      {/* <label className="absolute" htmlFor="search"> */}
+      {/*   Search */}
+      {/* </label> */}
       <MagnifyingGlassIcon
-        className="h-6 w-6 stroke-slate-600 sm:h-6 sm:w-6"
+        className="h-7 w-7 stroke-slate-600"
         aria-label="search icon"
       />
       <input
         aria-roledescription="search bar to filter the products"
         aria-label="search bar"
         role="search"
-        type="search"
-        className="w-full bg-transparent text-lg text-slate-600 selection:bg-sky-700 selection:text-white focus:outline-0 sm:w-[150px] md:w-[250px] lg:w-[300px]"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search for Products ..."
+        type="text"
+        id="search"
+        className="text-md w-full bg-transparent pr-7 text-slate-600 outline-0 focus:outline-0 sm:text-lg "
+        defaultValue={searchParams.get("query") || ""}
+        onChange={(e) => {
+          if (crossRef.current) {
+            if (e.target.value.length !== 0) {
+              crossRef.current.dataset.showCross = "true";
+            } else {
+              crossRef.current.dataset.showCross = "";
+            }
+          }
+          handleSearch(e.target.value);
+        }}
+        placeholder={placeholder}
         ref={inputRef}
+        autoComplete="off"
         onFocus={() => inputRef.current?.select()}
+      />
+      <XCircleIcon
+        ref={crossRef}
+        className="absolute right-5 hidden h-7 w-7 cursor-pointer fill-slate-500 transition-colors duration-300 hover:fill-slate-600 data-[show-cross=true]:block"
+        onClick={() => {
+          if (inputRef.current && crossRef.current) {
+            inputRef.current.value = "";
+            crossRef.current.dataset.showCross = "";
+            inputRef.current.focus();
+            handleSearch("");
+          }
+        }}
       />
     </div>
   );
